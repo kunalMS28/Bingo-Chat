1bdbcdc988066904968f47d95a6d848a897ad79d
kunalMS28 <156431240+kunalMS28@users.noreply.github.com>
2024-12-30 23:56:25 +0530

    Update tailwind.config.js 3

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx}',
@@ -74,5 +74,5 @@ export default {
     },
   },
   
-  plugins: [tailwindcssAnimate],
+  plugins: [require("tailwindcss-animate")],
 }
